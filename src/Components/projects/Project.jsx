17f7a0075fce2e341cba2744d87be644db3a8f27
Jacqline <jacqlinegeng@gmail.com>
2023-09-10 01:48:11 -0400

    modified route to project page

--- src/Components/projects/Project.jsx
+++ src/Components/projects/Project.jsx
@@ -4,7 +4,7 @@ import Projects from './Projects';
 
 const Project = () => {
   return (
-    <section className="work section" id='portfolio'>
+    <section className="work section" id='projects'>
       <h2 className="section__title">Projects</h2>
       <span className="section__subtitle">Check out what I have been working on recently</span>
       <Projects />
