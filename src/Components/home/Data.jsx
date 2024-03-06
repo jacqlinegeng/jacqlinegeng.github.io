337336a5af1c3d8c5c6e2266310c25d37fb91bb2
Jacqline <jacqlinegeng@gmail.com>
2024-03-05 23:32:40 -0500

    background update

--- src/Components/home/Data.jsx
+++ src/Components/home/Data.jsx
@@ -8,48 +8,53 @@ const Data = () => {
         <div className="home__img"></div>
       </h1>
       <p className="home__spacing">
-      <p className="home__description">
-        I build and explore everything about the human connection and human
-        consciousness.
-      </p>
-      </p>
-      <p className="home__spacing">
-      <p className="home__description">My ethos:</p>
-      <p className="home__description">
-        learn at a high velocity - speed and direction both matter
-      </p>
-      <p className="home__description">fail fast, then reflect and grow</p>
-      <p className="home__description">
-        don't compare; everyone was dealt a different set of cards, so focus on
-        your own hand
-      </p>
-      <p className="home__description">
-        optimize for the experience, not the outcome
-      </p>
-      <p className="home__description">
-        keep a beginner's mindset and stay humble
-      </p>
-      <p className="home__description">
-        life is an adventure, and love is a conscious choice
-      </p>
-      <p className="home__description">be exotic, be iconic!</p>
+        <p className="home__description">
+          I build and explore everything about the human connection and human
+          consciousness.
+        </p>
       </p>
       <p className="home__spacing">
-      <p className="home__description">
-        I am a senior consultant at Deloitte. I am working on my podcast about
-        personal growth and my healthcare project in demystifying medical bills
-        using LLM. Currently I'm interested in neurotech, mental health, venture
-        capital, longevity, psychedelic therapy, creative writing, and
-        electronic music production. Previously, I shipped features and
-        engineered developer documentation at thirdweb. I have a background in
-        business and information systems. I self-taught coding, blockchain
-        development, and psychology.
-      </p>
+        <p className="home__description">My ethos:</p>
+        <p className="home__description">
+          - learn at a high velocity - speed and direction both matter
+        </p>
+        <p className="home__description">- fail fast, then reflect and grow</p>
+        <p className="home__description">
+          - don't compare; everyone was dealt a different set of cards, so focus
+          on your own hand
+        </p>
+        <p className="home__description">
+          - optimize for the experience, not the outcome
+        </p>
+        <p className="home__description">
+          - keep a beginner's mindset and stay humble
+        </p>
+        <p className="home__description">
+          - life is an adventure, and love is a conscious choice
+        </p>
+        <p className="home__description">- be exotic, be iconic!</p>
       </p>
       <p className="home__spacing">
-      <p className="home__description">
-        I dance, DJ, vlog, snowboard, and write for fun.
+        <p className="home__description">
+          I am a senior consultant at Deloitte. I am working on my podcast about
+          personal growth and my healthcare project in demystifying medical
+          bills using LLM. Currently I'm interested in neurotech, mental health,
+          venture capital, longevity, psychedelic therapy, creative writing, and
+          electronic music production. Previously, I shipped features and
+          engineered developer documentation at thirdweb. I have a background in
+          business and information systems. I self-taught coding, blockchain
+          development, and psychology.
+        </p>
       </p>
+      <p className="home__spacing">
+        <p className="home__description">
+          I dance, DJ, vlog, snowboard, and write for fun.
+        </p>
+      </p>
+      <p className="home__spacing">
+        <p className="home__description">
+          I am always looking for something new, let's be friends!{" "}
+        </p>
       </p>
     </div>
   );
