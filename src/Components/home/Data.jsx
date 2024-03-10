694dc85a1f6c9be2b4d5b41143ecf59b4bfee6a0
Jacqline <jacqlinegeng@gmail.com>
2024-03-10 14:18:36 -0400

    refactored code

--- src/Components/home/Data.jsx
+++ src/Components/home/Data.jsx
@@ -7,80 +7,63 @@ const Data = () => {
         Jacqline Geng
         <div className="home__img"></div>
       </h1>
-      <p className="home__spacing">
-        <p className="home__description">
-          I build and explore everything about the{" "}
-          <a
-            href="https://www.linkedin.com/posts/luzhangvc_artificialintelligence-humanconsciousness-activity-7133860301438951425-DLmz?utm_source=share&utm_medium=member_desktop
+      <p className="home__description">
+        I build and explore everything about the{" "}
+        <a
+          href="https://www.linkedin.com/posts/luzhangvc_artificialintelligence-humanconsciousness-activity-7133860301438951425-DLmz?utm_source=share&utm_medium=member_desktop
 "
-          >
-            human connection
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://www.oprah.com/own-podcasts/the-purpose-of-your-life-is-becoming-conscious
+        >
+          human connection
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://www.oprah.com/own-podcasts/the-purpose-of-your-life-is-becoming-conscious
 "
-          >
-            human consciousness
-          </a>
-          .
-        </p>
+        >
+          human consciousness
+        </a>
+        .
       </p>
-      <p className="home__spacing">
-        <p className="home__description">My ethos:</p>
-        <p className="home__description">
-          - learn at a high velocity - speed and direction both matter
-        </p>
-        <p className="home__description">- fail fast, then reflect and grow</p>
-        <p className="home__description">
+
+      <p className="home__description">
+        <p>My ethos:</p>
+        <p>- learn at a high velocity - speed and direction both matter</p>
+        <p>- fail fast, then reflect and grow</p>
+        <p>
           - don't compare; everyone was dealt a different set of cards, so focus
           on your own hand
         </p>
-        <p className="home__description">
-          - optimize for the experience, not the outcome
-        </p>
-        <p className="home__description">
-          - keep a beginner's mindset and stay humble
-        </p>
-        <p className="home__description">
-          - life is an adventure, and love is a conscious choice
-        </p>
-        <p className="home__description">- be exotic, be iconic!</p>
+        <p>- optimize for the experience, not the outcome</p>
+        <p>- keep a beginner's mindset and stay humble</p>
+        <p>- life is an adventure, and love is a conscious choice</p>
+        <p>- be exotic, be iconic!</p>
       </p>
-      <p className="home__spacing">
-        <p className="home__description">
-          I am a senior consultant at Deloitte. I am working on my podcast about
-          personal growth and my healthcare project in demystifying medical
-          bills using LLM. Currently I'm interested in neurotech, mental health,
-          venture capital, longevity, psychedelic therapy, creative writing, and
-          electronic music production. Previously, I shipped developer
-          documentation and curated the developer community at thirdweb. I have
-          a background in business and information systems. I self-taught
-          coding, blockchain development, and psychology.
-        </p>
+
+      <p className="home__description">
+        I am a senior consultant at Deloitte. I am working on my podcast about
+        personal growth and my healthcare project in demystifying medical bills
+        using LLM. Currently I'm interested in neurotech, mental health, venture
+        capital, longevity, psychedelic therapy, creative writing, and
+        electronic music production. Previously, I shipped developer
+        documentation and curated the developer community at thirdweb. I have a
+        background in business and information systems. I self-taught coding,
+        blockchain development, and psychology.
       </p>
-      <p className="home__spacing">
-        <p className="home__description">
-          I dance, DJ, vlog, snowboard, and write for fun.
-        </p>
+
+      <p className="home__description">
+        I dance, DJ, vlog, snowboard, and write for fun.
       </p>
-      <p className="home__spacing">
-        <p className="home__description">
-          I am always looking for something new, let's be{" "}
-          <a
-            href="https://linktr.ee/jacqlineee
+
+      <p className="home__description">
+        I am always looking for something new, let's be{" "}
+        <a
+          href="https://linktr.ee/jacqlineee
 "
-          >
-            friends!
-          </a>{" "}
-        </p>
+        >
+          friends!
+        </a>{" "}
       </p>
-      <p className="home__spacing">
-        <p className="home__description">
-          "What you do makes a difference, and you have to decide what kind of
-          difference you want to make." ˜ Jane Goodall
-        </p>
-      </p>
+
       <p className="spotify__playlist">
         <iframe
           style={{ border: "10px" }}
@@ -91,6 +74,10 @@ const Data = () => {
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
         ></iframe>
+      </p>
+      <p className="home__description">
+        "What you do makes a difference, and you have to decide what kind of
+        difference you want to make." ˜ Jane Goodall
       </p>
     </div>
   );
