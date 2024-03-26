8b2f77669f2b3bd8d6ed9994387338a5cd579bc0
Jacqline <jacqlinegeng@gmail.com>
2024-03-25 20:39:53 -0400

    refactored writing page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Header } from "./Components/header/Header";
 import About from "./Components/about/About";
 import Project from "./Components/projects/Project";
 import DarkMode from "./Components/darkmode/DarkMode";
+import Writing from "./Components/writing/Writing";
 import Pondering from "./Components/pondering/Pondering";
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
         <Home />
         <About />
         <Project />
+        <Writing />
         <Pondering />
       </main>
     </>
