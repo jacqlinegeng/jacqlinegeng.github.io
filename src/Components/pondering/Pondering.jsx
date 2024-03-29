ff38997b54b268dafc558ce84fdf6597bc284896
Jacqline <jacqlinegeng@gmail.com>
2024-03-29 11:40:44 -0400

    moved spotify

--- src/Components/pondering/Pondering.jsx
+++ src/Components/pondering/Pondering.jsx
@@ -2,9 +2,22 @@ import React from "react";
 
 const Pondering = () => {
   return (
-    <section className="" id='pondering'>
-  </section>
-    );
+    <section className="" id="pondering">
+      <p>Currently listening</p>
+      {/* <p className="spotify__playlist">
+        <iframe
+          title="spotify"
+          style={{ border: "10px" }}
+          src="https://open.spotify.com/embed/playlist/6QF6lbpdaWdZyjdfAuiPrc?utm_source=generator&theme=0"
+          width="60%"
+          height="100"
+          allowfullscreen="true"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      </p> */}
+    </section>
+  );
 };
 
 export default Pondering;
