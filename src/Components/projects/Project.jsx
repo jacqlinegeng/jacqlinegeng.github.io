8b03ae1f9674e210539c9ffa57b11740d7828328
Jacqline <jacqlinegeng@gmail.com>
2024-04-09 12:04:02 -0400

    changed projects to pondering

--- src/Components/projects/Project.jsx
+++ src/Components/projects/Project.jsx
@@ -5,7 +5,7 @@ import Projects from './Projects';
 const Project = () => {
   return (
     <section className="work section" id='projects'>
-      <h2 className="section__title">Projects</h2>
+      <h2 className="section__title">Currently Pondering</h2>
       <span className="section__subtitle">
         "What you do makes a difference, and you have to decide what kind of
         difference you want to make." ˜ Jane Goodall
