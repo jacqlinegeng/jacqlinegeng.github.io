6561633a54f533a9c57d19bb0d3a6fc277991754
Jacqline <jacqlinegeng@gmail.com>
2024-04-12 16:34:25 -0400

    added info for the writing page

--- src/Components/writing/Writing.jsx
+++ src/Components/writing/Writing.jsx
@@ -2,9 +2,82 @@ import React from "react";
 
 const Writing = () => {
   return (
-    <section className="" id='writing'>
-  </section>
-    );
+    <div className="home__data">
+      <h1 className="home__title">
+        Jacqline Geng
+        <div className="home__img"></div>
+      </h1>
+      <p className="home__description">
+        I build and explore everything about the{" "}
+        <a
+          href="https://www.linkedin.com/posts/luzhangvc_artificialintelligence-humanconsciousness-activity-7133860301438951425-DLmz?utm_source=share&utm_medium=member_desktop
+"
+          className="animated__link"
+        >
+          human connection
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://www.oprah.com/own-podcasts/the-purpose-of-your-life-is-becoming-conscious
+"
+          className="animated__link"
+        >
+          human consciousness
+        </a>
+        .
+      </p>
+
+      <p className="home__description">
+        <p>My ethos:</p>
+        <p>- learn at a high velocity; speed and direction both matter</p>
+        <p>- fail fast, learn faster</p>
+        <p>
+          - don't compare; everyone was dealt a different set of cards, so focus
+          on your own hand
+        </p>
+        <p>- optimize for the experience, not the outcome</p>
+        <p>- maintain a beginner's mindset and embrace humility</p>
+        <p>- life is an adventure, and love is a conscious choice</p>
+        <p>- be exotic, be iconic!</p>
+      </p>
+
+      <p className="home__description">
+        During my 9-5, I am a senior consultant at Deloitte. In my 5-9, I am usually found working on my podcast about
+        personal growth and my healthcare project in demystifying medical bills
+        using LLM. Currently I'm interested in neurotech, mental health, venture
+        capital, longevity, psychedelics therapy, creative writing, and
+        electronic music production. Previously, I crafted technical
+        documentation and curated the developer community at thirdweb. I have a
+        background in business and information systems. I self-taught coding,
+        blockchain development, and psychology.
+      </p>
+
+      <p className="home__description">
+        I dance, DJ, <a
+          href="https://www.youtube.com/@jacqline
+"
+          className="animated__link"
+        >vlog</a>, snowboard, and <a
+        href="https://substack.com/@jacqline
+"
+        className="animated__link"
+      >write</a> for fun.
+      </p>
+
+      <p className="home__description">
+        I am always looking for something new, let's be{" "}
+        <a
+          href="https://linktr.ee/jacqlineee
+"
+          className="animated__link"
+        >
+          friends!
+        </a>{" "}
+      </p>
+
+      
+    </div>
+  );
 };
 
 export default Writing;
