817be40e40721e95cc1c87ce67e17531b907cc64
Jacqline <jacqlinegeng@gmail.com>
2024-04-22 19:46:11 -0400

    removed toggle comment

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import DarkMode from "../darkmode/DarkMode";
 
 export const Header = () => {
-  /* Toggle Menu */
   const [Toggle, showMenu] = useState(false);
   return (
     <div>
