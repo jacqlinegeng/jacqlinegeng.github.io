21696633d63d9b016b4220e9c00d91ef057dfed5
Jacqline <jacqlinegeng@gmail.com>
2024-05-01 23:12:54 -0400

    removed imports

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import "./App.css";
 import Home from "./Components/home/Home";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import { Header } from "./Components/header/Header";
-import About from "./Components/about/About";
 import Project from "./Components/projects/Project";
-import DarkMode from "./Components/darkmode/DarkMode";
 import Writing from "./Components/writing/Writing";
 
 const App = () => {
