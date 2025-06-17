517d48084d75651d158174e8dc0d9f402330383d
Jacqline <jacqlinegeng@gmail.com>
2025-06-16 21:43:56 -0400

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+const repoName = 'jacqlinegeng.github.io';
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: `/${repoName}/`,
   server: {
     host: "::",
     port: 8080,
