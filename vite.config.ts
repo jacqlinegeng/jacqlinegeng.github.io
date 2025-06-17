8d7830c7dba4afe1a59f3df86caf9f26a1d363dd
Jacqline <jacqlinegeng@gmail.com>
2025-06-16 21:49:16 -0400

    index html

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const repoName = 'jacqlinegeng.github.io';
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: `/${repoName}/`,
+  base: '/jacqlinegeng.github.io/',
   server: {
     host: "::",
     port: 8080,
